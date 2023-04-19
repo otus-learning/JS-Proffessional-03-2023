594113754157d6dfe436a83d9f18c556def752ab
my <email@email.ru>
2023-04-19 15:07:44 +0300

    Another task solution (with Array.reduce and types)

--- src/promiseReducer.ts
+++ src/promiseReducer.ts
@@ -1,18 +1,24 @@
-const fn1 = () => {
+type PromiseFunc = () => Promise;
+type OurReduceFunc = (number, number) => number;
+
+const fn1 : PromiseFunc = () => {
 	console.log('fn1')
 	return Promise.resolve(1)
 };
 
-const fn2 = () => new Promise((resolve) => {
+const fn2 : PromiseFunc = () => new Promise((resolve) => {
 	console.log('fn2')
 	setTimeout(() => resolve(2), 1000)
 });
 
-const reduce = (memo, value) => {
+const reduce : OurResuceFunc = (memo : number, value : number) : number => {
 	console.log('reduce');
 	return memo * value;  
 }
 
+const asyncFunctions : Array<PromiseFunc> = [fn1, fn2];
+
+/*
 function promiseReduce(asyncFunctions, reduce, initialValue) {
 	let idx = 1;
 	return (asyncFunctions.length) ? new Promise((resolve) => {
@@ -24,3 +30,11 @@ function promiseReduce(asyncFunctions, reduce, initialValue) {
 };
 
 promiseReduce([fn1, fn2], reduce, 1).then(console.log);
+*/
+
+(asyncFunctions.reduce((prev, curr) => 
+	prev.then(memo => {
+		return new Promise(resolve => curr().then(data => resolve(reduce(memo, data))))
+	//Promise.resolve(1) do make initial value for our reduce function (not Array.reduce)
+	}), Promise.resolve(1)
+)).then(console.log);
